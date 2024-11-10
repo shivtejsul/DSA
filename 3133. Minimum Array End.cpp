2aa7268970a2787ad47a8dffa90f2e38ce4a2b5b
Shivtej Sul <shivtej0901@gmail.com>
2024-11-10 21:29:37 +0530

    leetcode 3133

---
+++ 3133. Minimum Array End.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+long minEnd(int n, int x) {
+    long result = x;
+    long remaining = n - 1;
+    long position = 1;
+    
+    while (remaining) {
+        if (!(x & position)) {
+            result |= (remaining & 1) * position;
+            remaining >>= 1;
+        }
+        position <<= 1;
+    }
+    
+    return result;
+
+}
+
+int main() {
+    int n1 = 3, x1 = 4;
+    int result1 = minLastElement(n1, x1);
+    cout << "Output for (n = 3, x = 4): " << result1 << std::endl;
+
+       
+    return 0;
+}
